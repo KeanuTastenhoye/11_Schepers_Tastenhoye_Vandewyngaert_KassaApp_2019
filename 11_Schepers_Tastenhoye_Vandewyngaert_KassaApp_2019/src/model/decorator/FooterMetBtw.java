e93f22251d3cf39e8136ba810129f5e221b767a9
Eline <elineschepers1999@gmail.com>
2019-12-18 14:35:27 +0100

    kortingschrijver

--- 11_Schepers_Tastenhoye_Vandewyngaert_KassaApp_2019/src/model/decorator/FooterMetBtw.java
+++ 11_Schepers_Tastenhoye_Vandewyngaert_KassaApp_2019/src/model/decorator/FooterMetBtw.java
@@ -1,4 +1,34 @@
 package model.decorator;
 
-public class FooterMetBtw {
+import domain.Artikel;
+import jxl.read.biff.BiffException;
+
+import java.io.IOException;
+import java.util.List;
+
+public class FooterMetBtw extends KassabonDecorator{
+    private  KassabonAbstract kassabon;
+    private  String text;
+
+    public FooterMetBtw(KassabonAbstract kassabon, String text) {
+        this.kassabon = kassabon;
+        this.text = text;
+    }
+
+    public FooterMetBtw(KassabonAbstract kassabon) {
+        this.kassabon = kassabon;
+        this.text = getText();
+    }
+
+    @Override
+    public String getText() {
+        //hier moet je naar de enum gaan
+        //prijs zonder koring + korting bedrag meoten hier komen
+        return "dit is hoe veel btw er is : \n";
+    }
+
+    @Override
+    public String print(List<Artikel> artList) throws BiffException, IOException {
+        return kassabon.print(artList) + getText();
+    }
 }
