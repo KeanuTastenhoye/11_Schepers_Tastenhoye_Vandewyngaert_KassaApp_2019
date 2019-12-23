b815addb410cae343d7ba22f54a2dc67faee22f5
Keanu Tastenhoye <keanu.tastenhoye@student.ucll.be>
2019-12-23 15:27:58 +0100

    brol

--- 11_Schepers_Tastenhoye_Vandewyngaert_KassaApp_2019/src/database/KortingLezer.java
+++ 11_Schepers_Tastenhoye_Vandewyngaert_KassaApp_2019/src/database/KortingLezer.java
@@ -17,12 +17,15 @@ import java.util.Scanner;
 public class KortingLezer {
 
     public HashMap<KortingEnum, ArrayList<String>> load() throws BiffException, IOException {
-        //File file = new File("11_Schepers_Tastenhoye_Vandewyngaert_KassaApp_2019\\11_Schepers_Tastenhoye_Vandewyngaert_KassaApp_2019\\src\\bestanden\\kortingStrategieProperties");
+
+
         File file = new File("11_Schepers_Tastenhoye_Vandewyngaert_KassaApp_2019\\src\\bestanden\\kortingStrategieProperties");
 
         HashMap<KortingEnum, ArrayList<String>> kortingen = new HashMap<>();
         String inhoud;
         //File file = new File("D:\\UCLL\\FASE_2\\OO Ontwerpen\\untitled\\src\\bestanden\\kortingStrategieProperties");
+
+        //BufferedReader reader = new BufferedReader(new FileReader(file));
 
         Scanner scannerFile = new Scanner(file);        // scanner voor File
         while (scannerFile.hasNextLine()) {                // voor elke lijn van het bestand
@@ -30,22 +33,15 @@ public class KortingLezer {
             scannerLijn.useDelimiter(", ");
             //System.out.println(scannerLijn.next());// scheidingstekens van verschillende delen in de huidige lijn
             String categorie = scannerLijn.next().toUpperCase();// eerste deel huidige lijn tot aan /
-            System.out.println("categorie "+categorie);
             String subcategorie = scannerLijn.next();                // tweede deel huidige lijn tot aan /
-            System.out.println(subcategorie);
             String percentage = scannerLijn.next();                // tweede deel huidige lijn tot aan /
-            //percentage=percentage.substring(0,percentage.length()-1);
-            System.out.println(percentage);
-            if (!kortingen.containsKey(KortingEnum.valueOf(categorie))||kortingen.get(KortingEnum.valueOf(categorie)).isEmpty()) {
+            if( !kortingen.containsKey(KortingEnum.valueOf(categorie))||kortingen.get(KortingEnum.valueOf(categorie)).isEmpty()) {
                 kortingen.put(KortingEnum.valueOf(categorie),new ArrayList<>());
             }
             kortingen.put(KortingEnum.valueOf(categorie), kortingen.get(KortingEnum.valueOf(categorie)));
 
             kortingen.get(KortingEnum.valueOf(categorie)).add(subcategorie);
             kortingen.get(KortingEnum.valueOf(categorie)).add(percentage);
-        }
-        for (KortingEnum e: kortingen.keySet()) {
-            System.out.println(e.toString());
         }
         return kortingen;
     }
